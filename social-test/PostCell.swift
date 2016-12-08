409dbf3a3389a31d4d686803c6461e083e899d74
5nilha <bwtdirect@gmail.com>
2016-12-08 03:57:15 -0500

    Completed Home UIViewTable

---
+++ social-test/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  social-test
+//
+//  Created by Fabio Quintanilha on 12/8/16.
+//  Copyright © 2016 Studio. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl:UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
